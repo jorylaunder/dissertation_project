8f27f45288767b406b6436f03e6a4233df198abd
jory launder <jorylaunder@gmail.com>
2025-12-22 17:17:46 +0000

    get only my BLE devices and fetch RSSI

--- Scanner.py
+++ Scanner.py
@@ -1,10 +1,25 @@
-import asyncio
-from bleak import BleakScanner
+import asyncio #allows asynchronous running
+from bleak import BleakScanner #bluetooth low energy (BLE) scanner
+
+#MAC addresses of my BLE beacons and given name
+wanted_devices = {"48:87:2D:9D:55:81": "Beacon Alpha"
+                  #more to add
+                  }
+
+#called whenever a BLE packet is received
+def processBLEpacket(device, advertisement_data):
+    #prints name and RSSI of my BLE beacons only
+    if device.address in wanted_devices:
+        name = wanted_devices[device.address]
+        print(f"{name} - RSSI: {advertisement_data.rssi}")
 
 async def main():
-    #scans for all devices emitting bluetooth signals
-    devices = await BleakScanner.discover()
-    for device in devices:
-        print(device)
+    #create scanner object
+    #whenever a BLE signal is received call detection_callback
+    scanner = BleakScanner(processBLEpacket)
+    await scanner.start()
+    #continuously scan
+    while True:
+        await asyncio.sleep(1)
 
 asyncio.run(main())
