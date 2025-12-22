57cbf2b335d410b4c488ffa69a6e9c5c4b18ca42
jory launder <jorylaunder@gmail.com>
2025-12-22 16:00:37 +0000

    Scans for all bluetooth signals

--- Scanner.py
+++ Scanner.py
@@ -1,1 +1,10 @@
+import asyncio
+from bleak import BleakScanner
 
+async def main():
+    #scans for all devices emitting bluetooth signals
+    devices = await BleakScanner.discover()
+    for device in devices:
+        print(device)
+
+asyncio.run(main())
